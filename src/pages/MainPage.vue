f83d44362f525bdcab1bb168767bd6b1c074f485
mawenjie <754825416@qq.com>
2018-03-27 11:30:05 +0800

    'add_koa_axios'

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="MainPage">
-    123
+     <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
